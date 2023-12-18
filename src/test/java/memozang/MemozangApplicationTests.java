2889ef46ab763491f5200794469495d67c642d53
k1mjunyoung <vovrwm@gmail.com>
2023-12-18 11:43:20 +0900

    Initial commit

---
+++ src/test/java/memozang/MemozangApplicationTests.java
@@ -0,0 +1,13 @@
+package memozang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemozangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
