a47d777d0b32cd7fe83c1492719922be4c8a9f5d
k1mjunyoung <vovrwm@gmail.com>
2023-12-20 17:42:28 +0900

    feat: Logger 추가

--- src/main/java/memozang/TestController.java
+++ src/main/java/memozang/TestController.java
@@ -1,5 +1,7 @@
 package memozang;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("test")
 public class TestController {
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping("index")
     public String index() {
