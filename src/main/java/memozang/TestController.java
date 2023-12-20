4c3d0f14c9da0b07297687792ba5eecfc7ac47ba
k1mjunyoung <vovrwm@gmail.com>
2023-12-20 17:34:46 +0900

    feat: 테스트용 컨트롤러 추가

---
+++ src/main/java/memozang/TestController.java
@@ -0,0 +1,15 @@
+package memozang;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("test")
+public class TestController {
+
+    @GetMapping("index")
+    public String index() {
+        return "index";
+    }
+}
